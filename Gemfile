e5d958eb00a7c6bcba6e923f7887183f1ef7807f
Gildas Frémont <gildas.fremont@gmail.com>
2024-11-13 06:58:21 +0100

    Add Item model with image upload and basic CRUD

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem "view_component"
 # Storage
 gem "aws-sdk-s3", require: false
 
+# Active Storage Validations
+gem "active_storage_validations"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,21 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   validates :title, presence: true
-  validates :image, content_type: ['image/png', 'image/jpeg', 'image/jpg']
   
-  enum status: {
+  enum :status, {
     available: 0,    # Disponible
     borrowed: 1,     # Emprunté
     unavailable: 2   # Non disponible
-  }
+  }, default: :available
+  
+  # Validation de l'image si elle est présente
+  validates :image,
+            content_type: { in: ['image/png', 'image/jpg', 'image/jpeg'], message: 'doit être au format PNG ou JPEG' },
+            if: :image_attached?
+
+  private
+
+  def image_attached?
+    image.attached?
+  end
 end
